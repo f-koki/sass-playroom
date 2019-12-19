63fde1b797cbaedfad840acc17eee68ee1f213c1
f-koki <fukushimakoki0108@gmail.com>
2019-12-20 00:00:28 +0900

    feat: add classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       {/* TODO: You can change from here */}
-      <div className="box">hoge</div>
+      <header className="header"></header>
+      <div className="main"></div>
+      <footer className="footer"></footer>
       {/* You can change to here */}
     </div>
   );
-}
+};
 
 export default App;
