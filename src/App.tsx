3cf85e42ebf6a1b3c3cc19ebe2a9c0937effa142
f-koki <fukushimakoki0108@gmail.com>
2019-12-20 00:00:28 +0900

    feat: create boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       {/* TODO: You can change from here */}
-      <div className="box">hoge</div>
+      <div className="box-1"></div>
+      <div className="box-2"></div>
+      <div className="box-3"></div>
       {/* You can change to here */}
     </div>
   );
-}
+};
 
 export default App;
